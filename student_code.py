e9a2c16e45abac02aeacd391f1baa174af793d82
gmp324 <gmp324@lehigh.edu>
2025-11-02 00:54:38 +0000

    fixing some inheritance issues and variable usage issues

--- student_code.py
+++ student_code.py
@@ -1,41 +1,45 @@
-"""Creating TraversableDigraph and DAG classes with some inheritance dependencies"""
-from SortableDigraph import SortableDigraph #for inheritance
-from collections import deque #for queue structure in bfs
+"""TraversableDigraph and DAG implementations, extending SortableDigraph."""
+from collections import deque #for queue structure bfs
+from SortableDigraph import SortableDigraph
 
 class TraversableDigraph(SortableDigraph):
     """augments SortableDigraph with BFS and DFS methods"""
 
+    def get_nodes(self):
+        """Return a list of nodes in the graph."""
+        return list(self.graph.keys())
+
     def dfs(self, start):
-        """Depth-First Search...based on Listiing 5-5 from Python Algs."""
-        visited, stack = set(), [start] #visited and set queue
+        """Depth-First Search...based on Listing 5-5 from Python Algs."""
+        visited, stack = set(), [start] #replace listing variables for more clarity
         while stack:
             u = stack.pop()
             if u in visited:
                 continue
             visited.add(u)
-            stack.extend(self.graph.get(u, []))
+            stack.extend(v for v, _ in self.graph.get(u, {}).values())
             yield u
 
     def bfs(self, start):
         """Breadth-First Search...based on Listing 5-6 from Python Algs."""
-        visited, queue=set(), deque([start])
+        visited, queue = set(), deque([start])
         while queue:
-            u = u.queue.popleft()
+            u = queue.popleft()
             if u in visited:
                 continue
             visited.add(u)
-            for v in self.graph.get(u, []):
+            for v in self.graph.get(u, {}):
                 queue.append(v)
             yield u
 
 class DAG(TraversableDigraph):
     """DAG...to prevent graph cycles"""
 
-    def add_edge(self, u,v):
+    def add_edge(self, u, v, edge_name=None, weight=1, **kwargs):
         """adds edge from u to v, if a cycle is not created"""
-        if self._path_exists(v,u):
+        if self._path_exists(v, u):
             raise ValueError(f"Adding edge {u} -> {v} creates a cycle.")
-        super().add_edge(u, v)
+        super().add_edge(u, v, edge_name=edge_name, weight=weight)
 
     def _path_exists(self, start, target):
         """helper method to return true if target is reachable from start"""
